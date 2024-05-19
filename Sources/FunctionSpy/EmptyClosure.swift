5e321d5285286a884ffc8761b0f9946290fd93ae
twof <fabiobean2@gmail.com>
2024-05-19 16:16:18 -0700

    add async throws variants to empty closure

--- Sources/FunctionSpy/EmptyClosure.swift
+++ Sources/FunctionSpy/EmptyClosure.swift
@@ -37,3 +37,23 @@ public func emptyClosure<A, B, C>(_ closure: (A, B, C) -> Void) -> @Sendable (A,
 public func emptyClosure<A, B, C, D>(_ closure: (A, B, C, D) -> Void) -> @Sendable (A, B, C, D) -> Void {
   { (_: A, _: B, _: C, _: D) in }
 }
+
+/// Produces an empty closure with the same signature as the passed function. Resulting closure does nothing but return `result`.
+public func emptyClosure<A, Result>(_ closure: (A) async throws -> Result, result: Result) -> @Sendable (A) async throws -> Result {
+  { (_: A) in result }
+}
+
+/// Produces an empty closure with the same signature as the passed function. Resulting closure does nothing but return `result`.
+public func emptyClosure<A, B, Result>(_ closure: (A, B) async throws -> Result, result: Result) -> @Sendable (A, B) async throws -> Result {
+  { (_: A, _: B) in result }
+}
+
+/// Produces an empty closure with the same signature as the passed function. Resulting closure does nothing but return `result`.
+public func emptyClosure<A, B, C, Result>(_ closure: (A, B, C) async throws -> Result, result: Result) async throws -> @Sendable (A, B, C) -> Result {
+  { (_: A, _: B, _: C) in result }
+}
+
+/// Produces an empty closure with the same signature as the passed function. Resulting closure does nothing but return `result`.
+public func emptyClosure<A, B, C, D, Result>(_ closure: (A, B, C, D) async throws -> Result, result: Result) -> @Sendable (A, B, C, D) async throws -> Result {
+  { (_: A, _: B, _: C, _: D) in result }
+}
