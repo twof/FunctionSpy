9365bf95afe3a57113e69273113848fe5ae36676
twof <fabiobean2@gmail.com>
2024-05-19 16:19:15 -0700

    add async throws variants to spy

--- Sources/FunctionSpy/FunctionSpy.swift
+++ Sources/FunctionSpy/FunctionSpy.swift
@@ -137,3 +137,74 @@ public func spy<A, B, C, D, Result>(
     return try closure(a, b, c, d)
   })
 }
+
+/// Wraps a function in a spy
+/// - Parameter closure: The function to wrap
+/// - Returns: The spy and and the wrapped function
+public func spy<Result>(
+  _ closure: @escaping @Sendable () async throws -> Result
+) -> (Spy, @Sendable () async throws -> Result) {
+  let spy = Spy()
+  return (spy, {
+    spy.increment()
+    spy.recordCall()
+    return try await closure()
+  })
+}
+
+/// Wraps a function in a spy
+/// - Parameter closure: The function to wrap
+/// - Returns: The spy and and the wrapped function
+public func spy<A, Result>(
+  _ closure: @escaping @Sendable (A) async throws -> Result
+) -> (Spy1<A>, @Sendable (A) async throws -> Result) {
+  let spy = Spy1<A>()
+  return (spy, { a in
+    spy.increment()
+    spy.recordCall(a)
+    return try await closure(a)
+  })
+}
+
+/// Wraps a function in a spy
+/// - Parameter closure: The function to wrap
+/// - Returns: The spy and and the wrapped function
+public func spy<A, B, Result>(
+  _ closure: @escaping @Sendable (A, B) async throws -> Result
+) -> (Spy2<A, B>, @Sendable (A, B) async throws -> Result) {
+  let spy = Spy2<A, B>()
+  return (spy, { a, b in
+    spy.increment()
+    spy.recordCall(a, b)
+    return try await closure(a, b)
+  })
+}
+
+/// Wraps a function in a spy
+/// - Parameter closure: The function to wrap
+/// - Returns: The spy and and the wrapped function
+public func spy<A, B, C, Result>(
+  _ closure: @escaping @Sendable (A, B, C) async throws -> Result
+) -> (Spy3<A, B, C>, @Sendable (A, B, C) async throws -> Result) {
+  let spy = Spy3<A, B, C>()
+  return (spy, { a, b, c in
+    spy.increment()
+    spy.recordCall(a, b, c)
+    return try await closure(a, b, c)
+  })
+}
+
+/// Wraps a function in a spy
+/// - Parameter closure: The function to wrap
+/// - Returns: The spy and and the wrapped function
+public func spy<A, B, C, D, Result>(
+  _ closure: @escaping @Sendable (A, B, C, D) async throws -> Result
+) -> (Spy4<A, B, C, D>, @Sendable (A, B, C, D) async throws -> Result) {
+  let spy = Spy4<A, B, C, D>()
+  return (spy, { a, b, c, d in
+    spy.increment()
+    spy.recordCall(a, b, c, d)
+    return try await closure(a, b, c, d)
+  })
+}
+
