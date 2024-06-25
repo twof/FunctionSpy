37dd23d1350f390307e976d2fb6be4768093c534
twof <fabiobean2@gmail.com>
2024-06-24 23:34:59 -0700

    add spy actors

--- Sources/FunctionSpy/Spy.swift
+++ Sources/FunctionSpy/Spy.swift
@@ -11,3 +11,18 @@ final public class Spy<each A>: SpyProtocol, @unchecked Sendable {
     self.callParams.append((repeat each a))
   }
 }
+
+/// Tracks function calls. See `SpyProtocol` for more.
+public actor SpyActor<each A>: SpyActorProtocol {
+  public private(set) var callCount = 0
+  public private(set) var callParams: [(repeat each A)] = []
+  
+  func increment() {
+    self.callCount += 1
+  }
+  
+  func recordCall(_ a: repeat each A) {
+    self.callParams.append((repeat each a))
+  }
+}
+
